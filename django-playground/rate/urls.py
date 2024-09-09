f249b970467a75b053b31f0b271889ec9a23b2c6
syy <syy319@sina.com>
2024-09-09 20:19:05 +0800

    change data schema

--- django-playground/rate/urls.py
+++ django-playground/rate/urls.py
@@ -1,16 +1,14 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import EventViewSet, UserViewSet, ResourceViewSet, UserTaskViewSet, AspectViewSet
+from .views import EventViewSet, ResourceViewSet, AspectViewSet, UserViewSet, UserScoreViewSet
 
-# Create a router and register our viewsets with it.
 router = DefaultRouter()
-router.register(r'events', EventViewSet)
-router.register(r'users', UserViewSet)
-router.register(r'resources', ResourceViewSet)
-router.register(r'aspect', AspectViewSet)
-router.register(r'user-tasks', UserTaskViewSet)
+router.register('events', EventViewSet, basename='events')
+router.register('resources', ResourceViewSet, basename='resources')
+router.register('aspects', AspectViewSet, basename='aspects')
+router.register('users', UserViewSet, basename='users')
+router.register('user-scores', UserScoreViewSet, basename='user-scores')
 
-# The API URLs are now determined automatically by the router.
 urlpatterns = [
     path('', include(router.urls)),
 ]
