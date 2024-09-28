9848474e323d81f44627a5fc6f643fd475add008
syy <syy319@sina.com>
2024-09-28 15:49:36 +0800

    user model old image del, rerender after user upload, remove adds from mobil nva

--- django-playground/core/models.py
+++ django-playground/core/models.py
@@ -10,4 +10,10 @@ class CustomUser(AbstractUser):
             print('aaa')
             self.set_password(self.password)
 
+        if self.pk:
+            old_user = CustomUser.objects.get(pk=self.pk)
+            if old_user.avatar and old_user.avatar != self.avatar:
+                old_user.avatar.delete(save=False);
+
+
         super().save(*args,**kwargs)
