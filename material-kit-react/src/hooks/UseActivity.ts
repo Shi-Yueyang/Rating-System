c009355a55ff2bb5955dfb335d3b426a203eec39
syy <syy319@sina.com>
2024-10-05 19:41:45 +0800

    Error message from UseActivity mutation, use onError from useMutation and mutate.

--- material-kit-react/src/hooks/UseActivity.ts
+++ material-kit-react/src/hooks/UseActivity.ts
@@ -1,14 +1,20 @@
-import { useQuery } from '@tanstack/react-query';
-import axios from 'axios';
+import { useMutation, useQuery } from '@tanstack/react-query';
+import axios, { AxiosError } from 'axios';
 
 export interface Activity {
-  id: number;
+  id?: number;
   name: string;
   dueDate: string;
 }
 
+interface Props {
+  accessToken: string|null;
+}
+
+const endPoint = 'http://127.0.0.1:8000/rate/events/';
+
 async function fetchActivities(): Promise<Activity[]> {
-  const response = await axios.get('http://127.0.0.1:8000/rate/events/');
+  const response = await axios.get(endPoint);
   return response.data;
 }
 
@@ -17,4 +23,24 @@ export function useActivities() {
     queryKey: ['activities'],
     queryFn: fetchActivities,
   });
+}
+
+export const useUpdateActivities = ({accessToken}:Props) =>{
+
+  const headers: Record<string, string> = {
+    'Content-Type': 'multipart/form-data',
+  };
+  if (accessToken) {
+    headers.Authorization = `Bearer ${accessToken}`;
+  }
+
+  return useMutation({
+    mutationFn:(activity:Activity) =>{
+      return axios.post(endPoint,activity,headers)
+    },
+    onError:(error:AxiosError) =>{
+      console.error("[useUpdateActivities onError]:", error.response?.data);
+      throw error;
+    }
+  })
 }
