a9eb74acd0d70b0216fb915186cb379ef5d946d2
Shi Yueyang <syy319@sina.com>
2024-11-14 16:44:57 +0800

    remove unused files

--- material-kit-react/src/paths.ts
+++ material-kit-react/src/paths.ts
@@ -2,11 +2,8 @@ export const paths = {
   home: '/',
   auth: { signIn: '/auth/sign-in', signUp: '/auth/sign-up', resetPassword: '/auth/reset-password' },
   dashboard: {
-    overview: '/dashboard',
     account: '/dashboard/account',
     customers: '/dashboard/customers',
-    integrations: '/dashboard/integrations',
-    settings: '/dashboard/settings',
     activity: '/dashboard/assignment', // newly added
   },
   createEvent:'/dashboard/assignment/create',
