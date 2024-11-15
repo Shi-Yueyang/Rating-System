994ef06a7788bbac408dc3e6cfd401e3b9b660e7
Shi Yueyang <syy319@sina.com>
2024-11-15 10:15:04 +0800

    fix popper bug

---
+++ material-kit-react/src/components/dashboard/layout/MainNav.tsx
@@ -0,0 +1,69 @@
+'use client';
+
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Box from '@mui/material/Box';
+import IconButton from '@mui/material/IconButton';
+import Stack from '@mui/material/Stack';
+import { List as ListIcon } from '@phosphor-icons/react/dist/ssr/List';
+
+import { usePopover } from '@/hooks/use-popover';
+
+import { MobileNav } from './mobile-nav';
+import { UserPopover } from './user-popover';
+import { useUser } from '@/hooks/use-user';
+
+export function MainNav(): React.JSX.Element {
+  const [openNav, setOpenNav] = React.useState<boolean>(false);
+  const {user } = useUser();
+  const userPopover = usePopover<HTMLDivElement>();
+  console.log("[MainNav] user username:"+user?.username)
+  console.log("[MainNav] user avatar:"+user?.avatar)
+  return (
+    <React.Fragment>
+      <Box
+        component="header"
+        sx={{
+          borderBottom: '1px solid var(--mui-palette-divider)',
+          backgroundColor: 'var(--mui-palette-background-paper)',
+          position: 'sticky',
+          top: 0,
+          zIndex: 'var(--mui-zIndex-appBar)',
+        }}
+      >
+        <Stack
+          direction="row"
+          spacing={2}
+          sx={{ alignItems: 'center', justifyContent: 'space-between', minHeight: '64px', px: 2 }}
+        >
+          <Stack sx={{ alignItems: 'center' }} direction="row" spacing={2}>
+            <IconButton
+              onClick={(): void => {
+                setOpenNav(true);
+              }}
+              sx={{ display: { lg: 'none' } }}
+            >
+              <ListIcon />
+            </IconButton>
+
+          </Stack>
+          <Stack sx={{ alignItems: 'center' }} direction="row" spacing={2}>
+            <Avatar
+              onClick={userPopover.handleOpen}
+              ref={userPopover.anchorRef}
+              src={user?.avatar}
+              sx={{ cursor: 'pointer' }}
+            />
+          </Stack>
+        </Stack>
+      </Box>
+      <UserPopover anchorEl={userPopover.anchorRef.current} onClose={userPopover.handleClose} open={userPopover.open} />
+      <MobileNav
+        onClose={() => {
+          setOpenNav(false);
+        }}
+        open={openNav}
+      />
+    </React.Fragment>
+  );
+}
