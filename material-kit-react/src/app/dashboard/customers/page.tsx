0ab9ae4a174fa4d118ef1ab5e9283fe4f7c83f19
syy <syy319@sina.com>
2024-11-25 20:12:38 +0800

    add StaffOrOrganizerGuard

--- material-kit-react/src/app/dashboard/customers/page.tsx
+++ material-kit-react/src/app/dashboard/customers/page.tsx
@@ -1,18 +1,18 @@
 import * as React from 'react';
 import type { Metadata } from 'next';
 import Stack from '@mui/material/Stack';
+
 import { config } from '@/config';
 import CustomerPage from '@/components/dashboard/customer/CustomerPage';
+import { StaffOrOrganizerGuard } from '@/components/dashboard/StaffOrOrganizerGuard';
 
 export const metadata = { title: `Customers | Dashboard | ${config.site.name}` } satisfies Metadata;
 export default function Page(): React.JSX.Element {
-
-
-
   return (
-    <Stack spacing={3}>
-      <CustomerPage />
-    </Stack>
+    <StaffOrOrganizerGuard>
+      <Stack spacing={3}>
+        <CustomerPage />
+      </Stack>
+    </StaffOrOrganizerGuard>
   );
 }
-
