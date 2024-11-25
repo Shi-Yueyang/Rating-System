4d53f5d6dfed0c0958fb2468dc0ab76fb5feb5d2
syy <syy319@sina.com>
2024-11-25 20:12:38 +0800

    add StaffOrOrganizerGuard

--- material-kit-react/src/app/dashboard/assignment/page.tsx
+++ material-kit-react/src/app/dashboard/assignment/page.tsx
@@ -1,16 +1,19 @@
-import ActivityPage from '@/components/dashboard/assignments/ActivityPage';
 import { Stack, Typography } from '@mui/material';
 
+import ActivityPage from '@/components/dashboard/assignments/ActivityPage';
+import { StaffOrOrganizerGuard } from '@/components/dashboard/StaffOrOrganizerGuard';
 
 export default function Page(): React.JSX.Element {
   return (
-    <Stack spacing={3}>
-      <div>
-        <Typography variant="h4" padding={3}>
-          活动信息
-        </Typography>
-      </div>
-      <ActivityPage></ActivityPage>
-    </Stack>
+    <StaffOrOrganizerGuard>
+      <Stack spacing={3}>
+        <div>
+          <Typography variant="h4" padding={3}>
+            活动信息
+          </Typography>
+        </div>
+        <ActivityPage></ActivityPage>
+      </Stack>
+    </StaffOrOrganizerGuard>
   );
 }
