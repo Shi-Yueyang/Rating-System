aec60477d75bd977fe15178994334886618190d6
Shi Yueyang <syy319@sina.com>
2024-11-28 16:15:22 +0800

    add home page redirect

--- material-kit-react/src/app/dashboard/page.tsx
+++ material-kit-react/src/app/dashboard/page.tsx
@@ -1,16 +1,19 @@
+
 import * as React from 'react';
 import type { Metadata } from 'next';
 import Grid from '@mui/material/Unstable_Grid2';
 
 import { config } from '@/config';
+import { redirect } from 'next/navigation';
+import { paths } from '@/paths';
 
 
 export const metadata = { title: `Overview | Dashboard | ${config.site.name}` } satisfies Metadata;
 
 export default function Page(): React.JSX.Element {
+  redirect(paths.dashboard.assignment.base);
   return (
     <Grid container spacing={3}>
- 
     </Grid>
   );
 }
