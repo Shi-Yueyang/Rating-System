78b680b9707120f9c77617243dd0970fe2f9f116
syy <syy319@sina.com>
2024-12-07 10:46:24 +0800

    add user progress bar

--- material-kit-react/src/components/dashboard/customer/customers-table.tsx
+++ material-kit-react/src/components/dashboard/customer/customers-table.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import * as React from 'react';
+import { LinearProgress, Tab } from '@mui/material';
 import Avatar from '@mui/material/Avatar';
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
@@ -16,33 +17,41 @@ import TableRow from '@mui/material/TableRow';
 import Typography from '@mui/material/Typography';
 import dayjs from 'dayjs';
 
-import { useSelection } from '@/hooks/use-selection';
 import { User } from '@/types/user';
+import { useSelection } from '@/hooks/use-selection';
 
-function noop(): void {
-  // do nothing
-}
-
-
+import { UserResource } from '../assignments/ActivityDetails';
 
 interface CustomersTableProps {
   count?: number;
   page?: number;
-  rows?: User[];
+  users?: User[];
+  userResources?: UserResource[];
   rowsPerPage?: number;
 }
 
 export function CustomersTable({
   count = 0,
-  rows = [],
+  users = [],
+  userResources = [],
   page = 0,
   rowsPerPage = 0,
 }: CustomersTableProps): React.JSX.Element {
   const rowIds = React.useMemo(() => {
-    return rows.map((customer) => customer.id);
-  }, [rows]);
-
+    return users.map((customer) => customer.id);
+  }, [users]);
   const { selected } = useSelection(rowIds);
+
+  const tasksForEachUser = users.map((user) => {
+    const taskNum = userResources.filter((userResource) => userResource.user === user.id).length;
+    return { user: user.id, taskNum };
+  });
+  const completedTasksForEachUser = users.map((user) => {
+    const completeTaskNum = userResources.filter(
+      (userResource) => userResource.user === user.id && userResource.score > 0
+    ).length;
+    return { user: user.id, completeTaskNum };
+  });
 
   return (
     <Card>
@@ -52,23 +61,44 @@ export function CustomersTable({
             <TableRow>
               <TableCell>姓名</TableCell>
               <TableCell>邮箱</TableCell>
+              <TableCell>评审进度</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
-            {rows.map((row) => {
-              const isSelected = selected?.has(row.id);
+            {users.map((user) => {
+              const isSelected = selected?.has(user.id);
 
               return (
-                <TableRow hover key={row.id} selected={isSelected}>
-
+                <TableRow hover key={user.id} selected={isSelected}>
                   <TableCell>
                     <Stack sx={{ alignItems: 'center' }} direction="row" spacing={2}>
-                      <Avatar src={row.avatar} />
-                      <Typography variant="subtitle2">{row.realname}</Typography>
+                      <Avatar src={user.avatar} />
+                      <Typography variant="subtitle2">{user.realname}</Typography>
                     </Stack>
                   </TableCell>
-                  <TableCell>{row.email}</TableCell>
+                  <TableCell>{user.email}</TableCell>
+                  <TableCell>
+                    {(() => {
+                      const completedTasks =
+                        completedTasksForEachUser.find((task) => task.user === user.id)?.completeTaskNum || 0;
+                      const totalTasks = tasksForEachUser.find((task) => task.user === user.id)?.taskNum || 0;
+                      const completionPercentage = totalTasks > 0 ? (completedTasks / totalTasks) * 100 : 0;
 
+                      return (
+                        <Stack direction="row" alignItems="center" spacing={1}>
+                          <LinearProgress
+                            color="primary"
+                            variant="determinate"
+                            value={completionPercentage}
+                            sx={{ width: '100px', height: '10px', borderRadius: '5px' }}
+                          />
+                          <Typography variant="body2" sx={{ minWidth: '50px', textAlign: 'right' }}>
+                            {`${completedTasks}/${totalTasks}`}
+                          </Typography>
+                        </Stack>
+                      );
+                    })()}
+                  </TableCell>
                 </TableRow>
               );
             })}
@@ -79,8 +109,8 @@ export function CustomersTable({
       <TablePagination
         component="div"
         count={count}
-        onPageChange={noop}
-        onRowsPerPageChange={noop}
+        onPageChange={() => {}}
+        onRowsPerPageChange={() => {}}
         page={page}
         rowsPerPage={rowsPerPage}
         rowsPerPageOptions={[5, 10, 25]}
